357a1d41e89894f635498e273b531234a584889b
momo-87 <momochristian87@gmail.com>
2023-09-21 11:58:38 +0100

    Configure routing and setup the routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
+import NotMatch from './routes/NotMatch';
+import MainPage from './routes/MainPage';
+import DetailsPage from './routes/DetailsPage';
+import Layout from './components/Layout';
+
 import './App.css';
 
 const App = () => (
   <div className="App">
-    Learn React
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<MainPage />} />
+        <Route path="details" element={<DetailsPage />} />
+        <Route path="*" element={<NotMatch />} />
+      </Route>
+    </Routes>
   </div>
 );
 
---
+++ src/components/NavigationPanel.jsx
@@ -0,0 +1,5 @@
+const NavigationPanel = () => (
+  <div className="border-r h-screen w-[16%] pt-[100px]">Navigation Panel</div>
+);
+
+export default NavigationPanel;
