da3641e478cd8b995cf33c93cba63f842025a385
momo-87 <momochristian87@gmail.com>
2023-09-21 10:35:07 +0100

    Install and configure Taildwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "color-green": "#97BF0F",
+        "color-gray": "#E4E5E9",
+    },
+    },
+  },
+  plugins: [],
+}
+
