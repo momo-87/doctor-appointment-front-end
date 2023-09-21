3248b2eb4c62fd905ed4a2f580fbc519e74e3c39
momo-87 <momochristian87@gmail.com>
2023-09-21 13:30:40 +0100

    Styling the Navigation panel

--- src/components/NavigationPanel.jsx
+++ src/components/NavigationPanel.jsx
@@ -1,5 +1,30 @@
+import { NavLink } from 'react-router-dom';
+import '../styles/navigation.css';
+
+const links = [
+  { path: 'doctors', text: 'DOCTOR' },
+  { path: 'new-appointement', text: 'NEW APPOINTEMENT' },
+  { path: 'my-appointements', text: 'MY APPOINTEMENTS' },
+  { path: 'add-doctor', text: 'ADD DOCTOR' },
+  { path: 'delete-doctor', text: 'DELETE DOCTOR' },
+];
+
 const NavigationPanel = () => (
-  <div className="border-r h-screen w-[16%] pt-[100px]">Navigation Panel</div>
+  <div className="border-r h-screen w-auto pt-[100px]">
+    <aside>
+      <ul className="space-y-2 font-medium items-start flex flex-col ml-3 gap-2">
+        {links.map((link) => (
+          <li key={link.text} className={`pl-5 active:bg-color-green active:text-white hover:bg-color-green hover:text-white h-[40px] flex items-center w-full pr-5 font-bold ${({ isActive }) => (isActive ? 'active' : '')}`}>
+            <NavLink
+              to={link.path}
+            >
+              {link.text}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  </div>
 );
 
 export default NavigationPanel;
