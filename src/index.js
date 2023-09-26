6c6c608da49c65dc35cb72357fafae317665f3e6
abel-tefera <abeltefera16@gmail.com>
2023-09-26 11:54:11 +0300

    Persist redux store to local storage

--- src/index.js
+++ src/index.js
@@ -4,13 +4,18 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
-import { store } from './redux/store';
+import { persistor, store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
-  </Provider>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <App />
+        </Router>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import authReducer from './auth/authSlice';
+import appointmentReducer from './appointment/appointmentSlice';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import thunk from 'redux-thunk';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  appointment: appointmentReducer,
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-  },
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: [thunk]
 });
+
+export const persistor = persistStore(store)
+
