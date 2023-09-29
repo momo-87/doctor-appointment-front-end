0d4d8bc692c4fc89087c831c11ddcf41c5f582d2
momo-87 <momochristian87@gmail.com>
2023-09-29 20:00:41 +0100

    remove commented line from goupingDoctors function

--- src/logics/groupingDoctors.js
+++ src/logics/groupingDoctors.js
@@ -7,16 +7,5 @@ const groupingDoctors = (arr1, arr2) => {
   }
   return result;
 };
-// const groupingDoctors = (arr1, arr2) => {
-//   const result = [];
-//   arr2 = arr2.filter(value => !arr1.includes(value));
-//   for( let i = 0; i < arr2.length; i += 3) {
-//     tempArr = [];
-//     for (let j = i; j < i + 3; j += 1) {
-//       if(arr2[j]) tempArr.push(arr2[j]);
-//     }
-//     result.push(tempArr);
-//   }
-//   return result;
-// };
+
 export default groupingDoctors;
