071e3cfed9a2d86f342bf810408b32dd91b7681b
ragan <raganalemu3@gmail.com>
2023-10-02 13:29:22 +0300

    Add padding to bottom

--- src/routes/DeleteDoctors.jsx
+++ src/routes/DeleteDoctors.jsx
@@ -6,11 +6,11 @@ import { getDoctors } from '../redux/doctor/doctorSlice';
 function DeleteDoctorList() {
   const doctors = useSelector(getDoctors);
   return (
-    <div className=" pt-[20px] px-[10px] md-[20px]  md:px-[40px] lg:px-[60px]">
+    <div className=" py-[20px] px-[10px] md-[20px]  md:px-[40px] lg:px-[60px]">
       <h1 className="text-4xl my-4 text-center mt-[50px] mb-[20px]">
         Doctors List
       </h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 md:gap-20 ">
+      <div className="pb-[50px] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 md:gap-20 ">
         {doctors !== null
           && doctors.map((doctor) => (
             <DeleteDoctor key={doctor.id} doctor={doctor} />
