390ffe19d04167992740ae97644287e4b78cab74
ragan <raganalemu3@gmail.com>
2023-10-02 12:21:39 +0300

    Fix linter

--- src/routes/DeleteDoctors.jsx
+++ src/routes/DeleteDoctors.jsx
@@ -1,16 +1,16 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import DeleteDoctor from "../components/DeleteDoctor";
-import { getDoctors } from "../redux/doctor/doctorSlice";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import DeleteDoctor from '../components/DeleteDoctor';
+import { getDoctors } from '../redux/doctor/doctorSlice';
 
 function DeleteDoctorList() {
   const doctors = useSelector(getDoctors);
   return (
     <div className=" pt-[20px] px-[10px] md-[20px]  md:px-[40px] lg:px-[60px]">
-      <h1 className="text-4xl my-4 text-center">Doctors List</h1>
+      <h1 className="text-4xl my-4 text-center mb-[20px]">Doctors List</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 md:gap-20 ">
-        {doctors !== null &&
-          doctors.map((doctor) => (
+        {doctors !== null
+          && doctors.map((doctor) => (
             <DeleteDoctor key={doctor.id} doctor={doctor} />
           ))}
       </div>
