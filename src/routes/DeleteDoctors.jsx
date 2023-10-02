f78a9a991dbc62ee43c8b91b053b311b323850ed
ragan <raganalemu3@gmail.com>
2023-10-02 12:30:59 +0300

    Add margin for h1

--- src/routes/DeleteDoctors.jsx
+++ src/routes/DeleteDoctors.jsx
@@ -7,7 +7,9 @@ function DeleteDoctorList() {
   const doctors = useSelector(getDoctors);
   return (
     <div className=" pt-[20px] px-[10px] md-[20px]  md:px-[40px] lg:px-[60px]">
-      <h1 className="text-4xl my-4 text-center mb-[20px]">Doctors List</h1>
+      <h1 className="text-4xl my-4 text-center mt-[50px] mb-[20px]">
+        Doctors List
+      </h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 md:gap-20 ">
         {doctors !== null
           && doctors.map((doctor) => (
