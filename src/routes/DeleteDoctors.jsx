dcdfb33118b712285195665b91250a8c77031e59
ragan <raganalemu3@gmail.com>
2023-10-03 10:26:29 +0300

    refactor code

--- src/routes/DeleteDoctors.jsx
+++ src/routes/DeleteDoctors.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import DeleteDoctor from '../components/DeleteDoctor';
-import { getDoctors } from '../redux/doctor/doctorSlice';
-
-function DeleteDoctorList() {
+import React from "react";
+import { useSelector } from "react-redux";
+import DeleteDoctor from "../components/DeleteDoctor";
+import { getDoctors } from "../redux/doctor/doctorSlice";
+const DeleteDoctorList = () => {
   const doctors = useSelector(getDoctors);
   return (
     <div className=" py-[20px] px-[10px] md-[20px]  md:px-[40px] lg:px-[60px]">
@@ -11,13 +10,13 @@ function DeleteDoctorList() {
         Doctors List
       </h1>
       <div className="pb-[50px] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 md:gap-20 ">
-        {doctors !== null
-          && doctors.map((doctor) => (
+        {doctors !== null &&
+          doctors.map((doctor) => (
             <DeleteDoctor key={doctor.id} doctor={doctor} />
           ))}
       </div>
     </div>
   );
-}
+};
 
 export default DeleteDoctorList;
